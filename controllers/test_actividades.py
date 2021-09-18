08732b89bb808ce458af880a393ecabbb7147d28
kevinmore26 <74431425+kevinmore26@users.noreply.github.com>
2021-09-17 21:42:11 -0500

    commit

--- controllers/test_actividades.py
+++ controllers/test_actividades.py
@@ -1,26 +1,40 @@
 from app import app
 from unittest import TestCase
 
+
 class TestActividadesController(TestCase):
     def setUp(self):
-        '''Es el método oque servirá para configrar mis test de esta clase'''
+        '''Es el metodo que servira para configurar mis tests de esta clase'''
         self.app = app.test_client()
-        # para que el servidor piense que el servidor se levantó, pero no es así
-    
+
     def test_get_actividades(self):
         respuesta = self.app.get('/actividades')
-        print(respuesta.json)
-        self.assertEqual(respuesta.json, dict(message=None,content=[{
-                "actividadId":1,
-                "actividadNombre":"ir a la playa"
-            },
-                {
-                "actividadId":2,
-                "actividadNombre":"ir a comer"
-            },
-            {
-                "actividadId":3,
-                "actividadNombre":"ir al cumple de mi abuela"
-            }]))
 
-        self.assertEqual(respuesta.status_code,201)
+        self.assertEqual(respuesta.json, dict(message=None, content=[{
+            "actividadId": 1,
+            "actividadNombre": "Ir a la playa"
+        },
+            {
+            "actividadId": 2,
+                "actividadNombre": "Cocinar"
+        },
+            {
+            "actividadId": 3,
+                "actividadNombre": "Ir al cumpleaños de mi abuela"
+        }]))
+
+        self.assertEqual(respuesta.status_code, 201)
+
+    def test_fail_post_actividades(self):
+        respuesta = self.app.post('/actividades')
+
+        self.assertEqual(respuesta.status_code, 400)
+        self.assertDictEqual(respuesta.json, dict(
+            message={'actividadNombre': 'Falta la actividadNombre'}))
+
+    def test_post_actividades(self):
+        respuesta = self.app.post(
+            '/actividades', json={'actividadNombre': 'Hacer la tarea de backend'})
+
+        self.assertEqual(respuesta.status_code, 201)
+        self.assertIsNotNone(respuesta.json)
